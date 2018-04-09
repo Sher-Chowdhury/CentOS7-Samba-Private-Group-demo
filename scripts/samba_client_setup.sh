d1d8c962b0e68ecb7f4c5a219804cf927eab430e
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-04-09 13:25:05 +0100

    Initial commit

---
+++ scripts/samba_client_setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo '##########################################################################'
+echo '############### About to run samba_client_setup.sh script ##################'
+echo '##########################################################################'
+
+yum -y install samba samba-client cifs-utils
+
+mkdir -p /mnt/backups
+
+groupadd --gid 2000 sambagroup
+usermod -aG sambagroup root
+
+# this checks for available shares
+#smbclient -L //samba-storage.local -U samba_user1 <<EOF
+#passowrd123
+#EOF
+
+# mount -t cifs -o user=samba_user1,password=password123 //samba-storage.local/export_rw /mnt/export/
+
+echo '//samba-storage.local/bckp_storage /mnt/backups  cifs  username=samba_user1,password=password123,soft,rw  0 0' >> /etc/fstab
+mount -a
+
+exit 0
---
+++ scripts/samba_server_setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo '##########################################################################'
+echo '##### About to run samba_server_setup.sh script ##################'
+echo '##########################################################################'
+
+
+yum -y install samba samba-client
+
+mkdir -p /samba/export_rw
+
+setsebool -P samba_export_all_ro on
+setsebool -P samba_export_all_rw on
+setsebool -P samba_share_nfs on
+
+semanage fcontext -at samba_share_t "/samba/export_rw(/.*)?"
+
+restorecon -R /samba/export_rw
+
+groupadd --gid 2000 sambagroup
+chown nobody:sambagroup /samba/export_rw
+chmod g+rwx /samba/export_rw
+
+firewall-cmd --permanent --add-service=samba
+systemctl restart firewalld
+
+# this is to avoid an alias. 
+mv /etc/samba/smb.conf /etc/samba/smb.conf-orig
+/bin/cp -f /vagrant/files/smb.conf /etc/samba/smb.conf
+
+useradd samba_user1
+usermod -aG sambagroup samba_user1
+usermod -aG sambagroup root
+
+
+smbpasswd -a samba_user1 <<EOF
+password123
+password123
+EOF
+
+systemctl enable smb
+systemctl start smb
+
+exit 0
